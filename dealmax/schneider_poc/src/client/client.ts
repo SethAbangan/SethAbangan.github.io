c5c3bf3391204a51573ec3497de52fde4bddf9e0
lucky92-cyber <132029324+lucky92-cyber@users.noreply.github.com>
2023-05-13 13:53:21 +0800

    3D web implementation

--- dealmax/schneider_poc/src/client/client.ts
+++ dealmax/schneider_poc/src/client/client.ts
@@ -6,12 +6,59 @@ const scene = new THREE.Scene()
 const gridHelper = new THREE.GridHelper(10, 10, 0xaec6cf, 0xaec6cf)
 scene.add(gridHelper)
 
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'; 
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+
+// Instantiate a loader
+const loader = new GLTFLoader();
+
+// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( '/examples/jsm/libs/draco/' );
+loader.setDRACOLoader( dracoLoader );
+
+
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'channel/glb.glb',
+	// called when the resource is loaded
+	function ( gltf ) {
+        
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
 )
+
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+scene.add( light );
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -23,9 +70,10 @@ const material = new THREE.MeshBasicMaterial({
     wireframe: true,
 })
 
-const cube = new THREE.Mesh(geometry, material)
-cube.position.set(0, 0.5, -10)
-scene.add(cube)
+// const cube = new THREE.Mesh(geometry, material)
+// cube.position.set(0, 0.9, -5)
+// cube.scale(new THREE.Vector3(.5,.9,.1))
+// scene.add(cube)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -68,52 +116,52 @@ animationScripts.push({
     },
 })
 
-//add an animation that moves the cube through first 40 percent of scroll
-animationScripts.push({
-    start: 0,
-    end: 40,
-    func: () => {
-        camera.lookAt(cube.position)
-        camera.position.set(0, 1, 2)
-        cube.position.z = lerp(-10, 0, scalePercent(0, 40))
-        //console.log(cube.position.z)
-    },
-})
+// //add an animation that moves the cube through first 40 percent of scroll
+// animationScripts.push({
+//     start: 0,
+//     end: 40,
+//     func: () => {
+//         camera.lookAt(cube.position)
+//         camera.position.set(0, 1, 2)
+//         cube.position.z = lerp(-10, 0, scalePercent(0, 40))
+//         //console.log(cube.position.z)
+//     },
+// })
 
-//add an animation that rotates the cube between 40-60 percent of scroll
-animationScripts.push({
-    start: 40,
-    end: 60,
-    func: () => {
-        camera.lookAt(cube.position)
-        camera.position.set(0, 1, 2)
-        cube.rotation.z = lerp(0, Math.PI, scalePercent(40, 60))
-        //console.log(cube.rotation.z)
-    },
-})
+// //add an animation that rotates the cube between 40-60 percent of scroll
+// animationScripts.push({
+//     start: 40,
+//     end: 60,
+//     func: () => {
+//         camera.lookAt(cube.position)
+//         camera.position.set(0, 1, 2)
+//         cube.rotation.z = lerp(0, Math.PI, scalePercent(40, 60))
+//         //console.log(cube.rotation.z)
+//     },
+// })
 
-//add an animation that moves the camera between 60-80 percent of scroll
-animationScripts.push({
-    start: 60,
-    end: 80,
-    func: () => {
-        camera.position.x = lerp(0, 5, scalePercent(60, 80))
-        camera.position.y = lerp(1, 5, scalePercent(60, 80))
-        camera.lookAt(cube.position)
-        //console.log(camera.position.x + " " + camera.position.y)
-    },
-})
+// //add an animation that moves the camera between 60-80 percent of scroll
+// animationScripts.push({
+//     start: 60,
+//     end: 80,
+//     func: () => {
+//         camera.position.x = lerp(0, 5, scalePercent(60, 80))
+//         camera.position.y = lerp(1, 5, scalePercent(60, 80))
+//         camera.lookAt(cube.position)
+//         //console.log(camera.position.x + " " + camera.position.y)
+//     },
+// })
 
-//add an animation that auto rotates the cube from 80 percent of scroll
-animationScripts.push({
-    start: 80,
-    end: 101,
-    func: () => {
-        //auto rotate
-        cube.rotation.x += 0.01
-        cube.rotation.y += 0.01
-    },
-})
+// //add an animation that auto rotates the cube from 80 percent of scroll
+// animationScripts.push({
+//     start: 80,
+//     end: 101,
+//     func: () => {
+//         //auto rotate
+//         cube.rotation.x += 0.01
+//         cube.rotation.y += 0.01
+//     },
+// })
 
 function playScrollAnimations() {
     animationScripts.forEach((a) => {
