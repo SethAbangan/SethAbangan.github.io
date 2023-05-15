3ff1e9d65a2f52568f75cd98a0a72ec4d8b0a562
SethAbangan <122713075+SethAbangan@users.noreply.github.com>
2023-05-15 20:16:21 +0800

    wip

--- schneider_poc/src/client/client.ts
+++ schneider_poc/src/client/client.ts
@@ -4,7 +4,8 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xB3B6B7)
+// scene.background = new THREE.Color(0xB3B6B7)
+
 // Instantiate a loader
 const loader = new GLTFLoader();
 
@@ -47,11 +48,16 @@ const camera = new THREE.PerspectiveCamera(
 const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
 scene.add(light);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setClearColor(0x000000, 0);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
 // Get the canvas element created by the renderer
-var canvas = renderer.domElement;
+const canvas = renderer.domElement;
+canvas.style.position = 'absolute';
+canvas.style.top = '0';
+canvas.style.left = '0';
 
 // Set the left position of the canvas
 // canvas.style.left = '300px';
@@ -129,92 +135,6 @@ animationScripts.push({
   },
 });
 
-// animationScripts.push({
-//   start: 30,
-//   end: 40,
-//   func: () => {
-    
-//     // globeModel.position.z = lerp(0, 10, scalePercent(0, 100));
-//     globeModel.position.x = (3);
-//     globeModel.position.z = (-1);
-//     globeModel.position.y = (-3);
-//     globeModel.scale.set(1, 1, 1)
-
-
-//     // globeModel.position.y = lerp(0, 5, scalePercent(0, 200));
-//     // globeModel.position.z = lerp(0, 15, scalePercent(25, 100));
-//     // canvas.style.bottom = lerp(0, 0, 0) +'';
-//     // camera.lookAt(new THREE.Vector3(5,0,7));
-//     camera.lookAt(globeModel.scale);
-//     // camera.position.set(5,0,7);
-//   },
-// });
-
-
-//add an animation that moves the cube through first 40 percent of scroll
-// animationScripts.push({
-//   start: 60,
-//   end: 80,
-//   func: () => {
-//     // globeModel.position.x = lerp(0, 1, scalePercent(0, 0));
-//     // globeModel.position.y = lerp(0, 1, scalePercent(0, 0));
-//     // canvas.style.top = lerp(0, 100, scalePercent(0, 100)) +'';
-//     // globeModel.position.z = lerp(0, 1, scalePercent(0, 0));
-//     camera.lookAt(globeModel.position);
-//     // camera.position.set(0, 1, 2);
-//   },
-// });
-// //add an animation that moves the cube through first 40 percent of scroll
-// animationScripts.push({
-//   start: 90,
-//   end: 100,
-//   func: () => {
-//     globeModel.position.x = lerp(0, 1, scalePercent(0, 0));
-//     globeModel.position.y = lerp(0, 1, scalePercent(0, 0));
-//     // canvas.style.top = lerp(0, 100, scalePercent(0, 100)) +'';
-//     // globeModel.position.z = lerp(0, 1, scalePercent(0, 0));
-//     camera.lookAt(globeModel.position);
-//     // camera.position.set(0, 1, 2);
-//   },
-// });
-
-//add an animation that rotates the cube between 40-60 percent of scroll
-// animationScripts.push({
-//   start: 0,
-//   end: 60,
-//   func: () => {
-//     // camera.lookAt(cube.position);
-//     camera.lookAt(globeModel.position);
-//     camera.position.set(0, 1, 2);
-//     // cube.rotation.z = lerp(0, Math.PI, scalePercent(40, 60));
-//     //console.log(cube.rotation.z)
-//   },
-// });
-
-// // add an animation that moves the camera between 60-80 percent of scroll
-// animationScripts.push({
-//   start: 60,
-//   end: 80,
-//   func: () => {
-//     camera.position.x = lerp(0, 5, scalePercent(60, 80));
-//     camera.position.y = lerp(1, 5, scalePercent(60, 80));
-//     camera.lookAt(globeModel.position);
-//     // camera.lookAt(cube.position);
-//     //console.log(camera.position.x + " " + camera.position.y)
-//   },
-// });
-
-//add an animation that auto rotates the cube from 80 percent of scroll
-// animationScripts.push({
-//   start: 80,
-//   end: 101,
-//   func: () => {
-//     //auto rotate
-//     // cube.rotation.x += 0.01;
-//     // cube.rotation.y += 0.01;
-//   },
-// });
-
 function playScrollAnimations() {
   animationScripts.forEach((a) => {
     if (scrollPercent >= a.start && scrollPercent < a.end) {
@@ -227,17 +147,17 @@ let scrollPercent = 0;
 
 document.body.onscroll = () => {
   //calculate the current scroll progress as a percentage
-  scrollPercent =
-    ((document.documentElement.scrollTop || document.body.scrollTop) /
-      ((document.documentElement.scrollHeight || document.body.scrollHeight) -
-        document.documentElement.clientHeight)) *
-    100;
-  (document.getElementById("scrollProgress") as HTMLDivElement).innerText =
-    "Scroll Progress : " + scrollPercent.toFixed(2);
+  // scrollPercent =
+  //   ((document.documentElement.scrollTop || document.body.scrollTop) /
+  //     ((document.documentElement.scrollHeight || document.body.scrollHeight) -
+  //       document.documentElement.clientHeight)) *
+  //   100;
+  // (document.getElementById("scrollProgress") as HTMLDivElement).innerText =
+  //   "Scroll Progress : " + scrollPercent.toFixed(2);
 };
 
 const stats = new Stats();
-// document.body.appendChild(stats.dom);
+document.body.appendChild(stats.dom);
 
 function animate() {
   requestAnimationFrame(animate);
