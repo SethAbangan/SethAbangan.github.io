28a06db51c186143ddedc43d165b4d2e94216004
felix2023hub <felixalquizalas2023@gmail.com>
2023-05-15 17:46:56 +0800

    with animation

--- schneider_poc/src/client/client.ts
+++ schneider_poc/src/client/client.ts
@@ -8,39 +8,73 @@ scene.background = new THREE.Color(0xe3e3e3);
 // Instantiate a loader
 const loader = new GLTFLoader();
 
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
 // Optional: Provide a DRACOLoader instance to decode compressed mesh data
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath("/examples/jsm/libs/draco/");
 loader.setDRACOLoader(dracoLoader);
-
+let houseAnimMixer: THREE.AnimationMixer;
+let houseAnims: THREE.AnimationClip[];
 let model: THREE.Object3D<THREE.Event>;
+let houseAnimControl: boolean;
+
 // Load a glTF resource
 loader.load(
   // resource URL
-  "houses.glb",
+  "OrangeHouses.glb",
 
   // called when the resource is loaded
-  function (gltf) {
+  function (gltf: {
+    scene: any;
+    animations: any;
+    scenes: any;
+    cameras: any;
+    asset: any;
+  }) {
     model = gltf.scene;
 
+    houseAnims = gltf.animations;
     gltf.animations; // Array<THREE.AnimationClip>
     model; // THREE.Group
     gltf.scenes; // Array<THREE.Group>
-    gltf.cameras; // Array<THREE.Camera>
+    gltf.cameras; // Array<THREE.Camera>s
     gltf.asset; // Object
-    model.scale.set(0.1, 0.1, 0.1);
-    model.position.set(0, 0, -1);
-    model.rotation.set(0.3, -0.5, 0);
+    model.scale.set(0.06, 0.06, 0.06);
+    model.position.set(0, -.3, 0);
+    model.rotation.set(0.03, -0.05, 0);
 
     model.receiveShadow = true;
+    houseAnimMixer = new THREE.AnimationMixer(model);
+
+    for (let i = 0; i < 3; i++) {
+      const animation = houseAnims[i];
+      console.log(animation);
+
+      // choose the animation by its index
+      const action = houseAnimMixer.clipAction(animation);
+      //action.play();
+      action.setDuration(5);
+      action.setLoop(THREE.LoopRepeat, Infinity).play();
+    }
+
     scene.add(model);
   },
   // called while loading is progressing
-  function (xhr) {
+  function (xhr: { loaded: number; total: number }) {
     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
   },
   // called when loading has errors
-  function (error) {
+  function () {
     console.log("An error happened");
   }
 ); // Load a glTF resource
@@ -63,31 +97,22 @@ loader.load(
 //   action.setLoop(THREE.LoopRepeat,Infinity).play();
 
 //   scene.add(model);
-//   const animate = function () { requestAnimationFrame(animate); mixer.update(0.01); // Update the animation mixer 
-//   renderer.render(scene, camera); 
-// }; 
+//   const animate = function () { requestAnimationFrame(animate); mixer.update(0.01); // Update the animation mixer
+//   renderer.render(scene, camera);
+// };
 // animate();
 // });
 //const gridHelper = new THREE.GridHelper(10, 10, 0xaec6cf, 0xaec6cf)
 //scene.add(gridHelper)
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
 // const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 0.3);
 // scene.add(light);
 
-const light = new THREE.DirectionalLight(0xffffff, 0.3);
+const light = new THREE.PointLight(0xffffff, 1, 500);
 light.castShadow = true; // enable shadow casting
 scene.add(light);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+light.position.set(1, 20, 0);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
@@ -105,6 +130,19 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
   render();
+}
+
+function animateHouse() {
+  if (houseAnimControl) return;
+
+  houseAnimControl = true;
+
+  const animate = function () {
+    requestAnimationFrame(animate);
+    houseAnimMixer.update(0.01); // Update the animation mixer
+    renderer.render(scene, camera);
+  };
+  animate();
 }
 
 /* Liner Interpolation
@@ -129,6 +167,23 @@ const animationScripts: { start: number; end: number; func: () => void }[] = [];
 //add an animation that flashes the cube through 100 percent of scroll
 animationScripts.push({
   start: 0,
+  end: 40,
+  func: () => {
+    // find another solution, call func only once
+    animateHouse();
+  },
+});
+animationScripts.push({
+  start: 40,
+  end: 50,
+  func: () => {
+    // find another solution, call func only once
+    animateHouse();
+  },
+});
+
+animationScripts.push({
+  start: 0,
   end: 101,
   func: () => {
     let g = material.color.g;
@@ -147,9 +202,9 @@ animationScripts.push({
   func: () => {
     camera.position.x = lerp(0, 2, scalePercent(0, 80));
     camera.position.y = lerp(1, 2, scalePercent(0, 80));
-    // camera.lookAt(cube.position);
+     //camera.lookAt(cube.position);
     camera.lookAt(model.position);
-    // camera.position.set(0, 1, 2);
+    camera.position.set(0, 0.5, 1);
     // cube.position.z = lerp(-10, 0, scalePercent(0, 40));
     //console.log(cube.position.z)
   },
@@ -162,7 +217,7 @@ animationScripts.push({
 //   func: () => {
 //     // camera.lookAt(cube.position);
 //     camera.lookAt(model.position);
-//     camera.position.set(0, 1, 2);
+     camera.position.set(0, 1, 2);
 //     // cube.position.z = lerp(-10, 0, scalePercent(0, 40));
 //     //console.log(cube.position.z)
 //   },
@@ -186,8 +241,8 @@ animationScripts.push({
 //   start: 60,
 //   end: 80,
 //   func: () => {
-//     camera.position.x = lerp(0, 5, scalePercent(60, 80));
-//     camera.position.y = lerp(1, 5, scalePercent(60, 80));
+     camera.position.x = lerp(0, 5, scalePercent(60, 80));
+     camera.position.y = lerp(1, 5, scalePercent(60, 80));
 //     // camera.lookAt(cube.position);
 //     camera.lookAt(model.position);
 //     //console.log(camera.position.x + " " + camera.position.y)
